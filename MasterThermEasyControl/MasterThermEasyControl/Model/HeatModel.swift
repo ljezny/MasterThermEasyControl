b9a0bcbcf5a165bf939b3aba56d1819a0c3fc975
Lukas Jezny <ljezny@gmail.com>
2019-06-22 13:16:56 +0200

    - constructor of heatmodel

--- MasterThermEasyControl/MasterThermEasyControl/Model/HeatModel.swift
+++ MasterThermEasyControl/MasterThermEasyControl/Model/HeatModel.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class HeatModel : TemperatureModelBase {
-    /*let real_temp_param_id:Int
     let has_real_temp_param_id:Int
+    let real_temp_param_id:Int
     let approximated_real_temp_param_id:Int
     let set_temp_param_id:Int
     let hw_set_temp_param_id:Int
     let hw_real_temp_param_id:Int
     let is_on_param_id:Int
     
-    init(real_temp_param_id:Int,has_real_temp_param_id:Int,approximated_real_temp_param_id:Int,set_temp_param_id:Int,hw_set_temp_param_id:Int,hw_real_temp_param_id:Int,is_on_param_id:Int) {
+    init(has_real_temp_param_id:Int,real_temp_param_id:Int,approximated_real_temp_param_id:Int,set_temp_param_id:Int,hw_set_temp_param_id:Int,hw_real_temp_param_id:Int,is_on_param_id:Int) {
         self.real_temp_param_id = real_temp_param_id
         self.has_real_temp_param_id = has_real_temp_param_id
         self.approximated_real_temp_param_id = approximated_real_temp_param_id
@@ -25,7 +25,7 @@ class HeatModel : TemperatureModelBase {
         self.hw_set_temp_param_id = hw_set_temp_param_id
         self.hw_real_temp_param_id = hw_real_temp_param_id
         self.is_on_param_id = is_on_param_id
-    }*/
+    }
     
     override var name: String? {
         return NSLocalizedString("heatmodel.name", comment: "")
